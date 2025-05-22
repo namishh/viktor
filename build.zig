3713a618e0b246cfbe5050414cdb55c652dffb5a
namish <namish@tuta.io>
2025-05-22 15:55:57 +0530

    bag of words

--- build.zig
+++ build.zig
@@ -33,6 +33,15 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
+    const bow_tests = b.addTest(.{
+        .root_source_file = b.path("src/bagofwords.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_bow_tests = b.addRunArtifact(bow_tests);
+
     const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_bow_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,29 @@
 const std = @import("std");
 
+pub fn VectorDB(comptime T: type) type {
+    return struct {
+        vectors: std.ArrayList([]T),
+        allocator: std.mem.Allocator,
+
+        pub fn init(allocator: std.mem.Allocator) !@This() {
+            return .{
+                .vectors = try std.ArrayList([]T).initCapacity(allocator, 16),
+                .allocator = allocator,
+            };
+        }
+
+        pub fn deinit(self: *@This()) void {
+            self.vectors.deinit();
+        }
+    };
+}
+
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var viktor = try VectorDB(u8).init(allocator);
+    defer viktor.deinit();
     std.debug.print("hello world", .{});
 }
