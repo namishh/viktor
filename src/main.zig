b98025d34ea1939c79cecdb6acdc0b49c7dd5e5f
namish <namish@tuta.io>
2025-05-22 17:33:48 +0530

    save

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const BagOfWords = @import("bagofwords.zig").BagOfWords;
 
 pub fn VectorDB(comptime T: type) type {
     return struct {
@@ -12,10 +13,22 @@ pub fn VectorDB(comptime T: type) type {
             };
         }
 
+        pub fn add(self: *@This(), vector: []T) !void {
+            try self.vectors.append(vector);
+        }
+
         pub fn deinit(self: *@This()) void {
             self.vectors.deinit();
         }
     };
+}
+
+fn setupBag(allocator: std.mem.Allocator) !BagOfWords {
+    var Bag = try BagOfWords.init(allocator);
+    const documents = [_][]const u8{ "hello world", "hello zig", "zig is great", "completely different string" };
+
+    try Bag.fit(&documents);
+    return Bag;
 }
 
 pub fn main() !void {
@@ -25,5 +38,20 @@ pub fn main() !void {
 
     var viktor = try VectorDB(u8).init(allocator);
     defer viktor.deinit();
+
+    var bag = try setupBag(allocator);
+    defer bag.deinit();
+
+    const string1 = "different world world";
+    const s = try bag.transform(string1);
+
+    std.debug.print("Transformed vector: ", .{});
+    for (s, 0..) |val, i| {
+        if (i > 0) std.debug.print(", ", .{});
+        std.debug.print("{}", .{val});
+    }
+    std.debug.print("\n", .{});
+    allocator.free(s);
+
     std.debug.print("hello world", .{});
 }
