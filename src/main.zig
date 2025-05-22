596f026004be036f19da2784ead72af977a71299
namish <namish@tuta.io>
2025-05-23 01:16:01 +0530

    safe

--- src/main.zig
+++ src/main.zig
@@ -1,28 +1,6 @@
 const std = @import("std");
 const BagOfWords = @import("bagofwords.zig").BagOfWords;
 
-pub fn VectorDB(comptime T: type) type {
-    return struct {
-        vectors: std.ArrayList([]T),
-        allocator: std.mem.Allocator,
-
-        pub fn init(allocator: std.mem.Allocator) !@This() {
-            return .{
-                .vectors = try std.ArrayList([]T).initCapacity(allocator, 16),
-                .allocator = allocator,
-            };
-        }
-
-        pub fn add(self: *@This(), vector: []T) !void {
-            try self.vectors.append(vector);
-        }
-
-        pub fn deinit(self: *@This()) void {
-            self.vectors.deinit();
-        }
-    };
-}
-
 fn setupBag(allocator: std.mem.Allocator) !BagOfWords {
     var Bag = try BagOfWords.init(allocator);
     const documents = [_][]const u8{ "hello world", "hello zig", "zig is great", "completely different string" };
@@ -35,9 +13,6 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-
-    var viktor = try VectorDB(u8).init(allocator);
-    defer viktor.deinit();
 
     var bag = try setupBag(allocator);
     defer bag.deinit();
