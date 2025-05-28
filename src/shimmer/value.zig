012f642375c09e379c96ff13c5ed04547917a44c
namish <namish@tuta.io>
2025-05-29 01:02:48 +0530

    comments #1 - value

--- src/shimmer/value.zig
+++ src/shimmer/value.zig
@@ -1,17 +1,32 @@
 const std = @import("std");
 const DatabaseError = @import("errors.zig").DatabaseError;
 
-// serialization code taken from https://github.com/ziglibs/s2s/
-
+// utility function that returns a type that is aligned to the size of T.
 fn AlignedInt(comptime T: type) type {
     return std.math.ByteAlignedInt(T);
 }
+
+// Value is a generic type that can serialize and deserialize any type T.
+// It takes in
+// - a type parameter `T` which is the type of the value to be stored.
+// - an optional allocator to manage memory for complex types, slices, structs, pointers.
+
+// much of the serialization logic is based on this repo https://github.com/ziglibs/s2s/
 
 pub fn Value(comptime T: type) type {
     return struct {
         data: T,
         allocator: ?std.mem.Allocator,
         const Self = @This();
+
+        // This function recursively serializes the value of type T into a stream by taking into account its type information.
+        // for primitive types, it writes the raw bytes directly.
+        // for complex types, it recursively serializes each field or element.
+        // It takes in
+        // - `stream`: the output stream to write the serialized data to.
+        // - `comptime T2`: the type of the value to be serialized.
+        // - `value`: the value of type T2 to be serialized.
+        // It returns an error if the serialization fails.
 
         fn serializeRecursive(stream: anytype, comptime T2: type, value: T2) !void {
             switch (@typeInfo(T2)) {
@@ -67,6 +82,14 @@ pub fn Value(comptime T: type) type {
             }
         }
 
+        // This function recursively deserializes data from a stream into a value of type T2.
+        // it reads the data based on the type information of T2 and populates the target pointer.
+        // It takes in
+        // - `stream`: the input stream to read the serialized data from.
+        // - `comptime T2`: the type of the value to be deserialized.
+        // - `allocator`: an optional allocator to manage memory for complex types, slices, structs, pointers.
+        // - `target`: a pointer to the target value of type T2 where the deserialized data will be stored.
+        // returns an error if the deserialization fails, such as unexpected data, out of memory, or end of stream.
         fn recursiveDeserialize(
             stream: anytype,
             comptime T2: type,
@@ -131,6 +154,11 @@ pub fn Value(comptime T: type) type {
             }
         }
 
+        // TODO: is there a way to avoid duplicating the data?
+        // This function serializes the value of type T into a byte array.
+        // It takes in
+        // - `allocator`: an allocator to manage memory for complex types, slices, structs, pointers.
+        // returns a byte array containing the serialized data or an error if serialization fails.
         pub fn convertToBytes(self: *const Self, allocator: *const std.mem.Allocator) ![]u8 {
             var list = std.ArrayList(u8).init(allocator.*);
             defer list.deinit();
@@ -139,6 +167,11 @@ pub fn Value(comptime T: type) type {
             return try allocator.dupe(u8, list.items);
         }
 
+        // This function deserializes a byte array into a Value of type T.
+        // It takes in
+        // - `bytes`: a byte array containing the serialized data.
+        // - `allocator`: an allocator to manage memory for complex types, slices, structs, pointers.
+        // It returns a Value containing the deserialized data or an error if deserialization fails.
         pub fn fromBytes(bytes: []const u8, allocator: std.mem.Allocator) !Self {
             var stream = std.io.fixedBufferStream(bytes);
             var result: T = undefined;
@@ -146,6 +179,13 @@ pub fn Value(comptime T: type) type {
             return Self{ .data = result, .allocator = allocator };
         }
 
+        // function to recursively deinitialize the value of type T. this is for pointers, slices, structs.
+        // it takes in
+        // - `allocator`: an allocator to manage memory for complex types, slices, structs, pointers.
+        // - `comptime T2`: the type of the value to be deinitialized.
+        // - `value`: a pointer to the value of type T2 to be deinitialized.
+        // it deinitializes the value by freeing memory for slices, pointers, and recursively deinitializing fields of structs.
+        // it does not free the memory for primitive types, as they are stack allocated.
         fn deinitRecursive(allocator: std.mem.Allocator, comptime T2: type, value: *const T2) void {
             switch (@typeInfo(T2)) {
                 .pointer => |ptr| {
@@ -169,6 +209,8 @@ pub fn Value(comptime T: type) type {
             }
         }
 
+        // this function deinitializes the value of type T.
+        // it frees memory for complex types, slices, structs, pointers if an allocator is provided.
         pub fn deinit(self: *const Self) void {
             if (self.allocator) |allocator| {
                 deinitRecursive(allocator, T, &self.data);
